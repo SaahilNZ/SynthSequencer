e6cde28804104d6eafbfd0478b64b2a4ef007e71
Saahil Hari <saahilh@outlook.co.nz>
2017-02-12 14:50:10 +1100

    Set up audio context and frequencies array.

---
+++ audio.js
@@ -0,0 +1,30 @@
+var bpm = 60;
+var context;
+var frequencies = [
+    261.64,
+    293.66,
+    329.63,
+    349.23,
+    392.00,
+    440.00,
+    493.88,
+    523.25,
+    587.33,
+    659.25,
+    698.46,
+    783.99,
+    880.00,
+    987.77,
+    1046.50
+];
+
+window.AudioContext = window.AudioContext||window.webkitAudioContext;
+context = new AudioContext();
+
+function startAudio() {
+    
+}
+
+function stopAudio() {
+    
+}
